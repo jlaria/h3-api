452f08dae53c5cc7ded2666c2ce1a93547c30e83
Juan C. Laria <juank.laria@gmail.com>
2025-01-19 13:49:50 +0100

    Inital h3 functions

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.12-bookworm
+
+ARG USERNAME=vscode
+ARG USER_UID=1000
+ARG USER_GID=$USER_UID
+
+# Create the user
+RUN groupadd --gid $USER_GID $USERNAME \
+    && useradd --uid $USER_UID --gid $USER_GID -m $USERNAME \
+    #
+    # [Optional] Add sudo support. Omit if you don't need to install software after connecting.
+    && apt-get update \
+    && apt-get install -y sudo git \
+    && echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME \
+    && chmod 0440 /etc/sudoers.d/$USERNAME
+
+COPY requirements.txt /tmp/requirements.txt
+RUN pip install -r /tmp/requirements.txt
+
+USER $USERNAME
--- main.py
+++ main.py
@@ -1,14 +1,59 @@
-from typing import Optional
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+import h3
 
-from fastapi import FastAPI
+import shapely.wkt
+from shapely.ops import transform
 
+# Create FastAPI instance
 app = FastAPI()
 
+# Define the request schema
+class PolyfillRequest(BaseModel):
+    wkt: str  
+    resolution: int
 
+
+# Define the response schema
+class PolyfillResponse(BaseModel):
+    h3_indexes: List[str]
+
+def cell_to_wkt(cell):
+    boundary = list(h3.cell_to_boundary(cell))
+    boundary.append(boundary[0])
+    poly = shapely.geometry.Polygon(boundary)
+    poly = transform(lambda x,y: (y,x), poly)
+    return poly.wkt
+
+@app.post("/polyfill", response_model=PolyfillResponse)
+async def polyfill(request: PolyfillRequest):
+    """
+    Endpoint to polyfill a wkt polygon with H3 indexes at a given resolution.
+    """
+    try:
+        # Validate resolution
+        if not (0 <= request.resolution <= 15):
+            raise HTTPException(
+                status_code=400, detail="Resolution must be between 0 and 15."
+            )
+
+        # Use the h3 library to polyfill the polygon
+        poly = shapely.wkt.loads(request.wkt)
+        poly = h3.geo_to_h3shape(poly)
+        cells = h3.polygon_to_cells(poly, request.resolution)
+        polygons = [cell_to_wkt(x) for x in cells]
+
+        return PolyfillResponse(h3_indexes=list(polygons))
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# Health check endpoint
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def health_check():
+    """
+    Health check endpoint to verify API is running.
+    """
+    return {"status": "ok", "message": "H3 Polyfill API is running."}
